fc7ed37df412a9136074d22241df5d2347c8a14b
Abraham Pech <abrahamayil415@gmail.com>
2025-07-15 19:19:23 -0600

    Optimizando funciones y código

--- index.js
+++ index.js
@@ -1,59 +1,60 @@
 //REGISTRO
-const arrowBack = document.getElementById("back");
-const backTest = document.getElementById("back-text");
 const signUp = document.getElementById("sign-up");
 const login = document.getElementById("log-in");
 const password = document.getElementById("contraseña");
 const passwordRpt = document.getElementById("contraseña-rpt");
 const showPassword = document.getElementById("eye-slash");
 const showPasswordRpt = document.getElementById("eye-slash-rpt");
-const btnSignUp = document.getElementById("btn-sign-up");
-const loginPwd = document.getElementById("login-pwd")
-const loginIcon = document.getElementById("login-pwd-icon")
+const loginPwd = document.getElementById("login-pwd");
+const inputsRegister = document.querySelectorAll(".inputs-register");
+const inputsLogin = document.querySelectorAll(".inputs-login");
 
-//Show components login and sign up
-arrowBack.addEventListener("click", () => {
+//Mostrando el login
+function showLogin() {
   signUp.style.display = "none";
   login.style.display = "block";
-});
 
-backTest.addEventListener("click", () => {
-  signUp.style.display = "none";
-  login.style.display = "block";
-});
+  inputsRegister.forEach((input) => {
+    input.value = "";
+  });
+}
 
-//Show original password
-showPassword.addEventListener("click", () => {
+//Dirigirse al componente de registro
+function showRegister() {
+  login.style.display = "none";
+  signUp.style.display = "flex";
+
+  inputsLogin.forEach((input) => {
+    input.value = "";
+  });
+}
+
+//Mostrando contraseña sin ocultar
+function revealPassword() {
   const isPassword = password.type == "password";
 
   password.type = isPassword ? "text" : "password";
   showPassword.classList.toggle("fa-eye");
   showPassword.classList.toggle("fa-eye-slash");
-});
+}
 
-//Show repeat password
-showPasswordRpt.addEventListener("click", () => {
+//Mostrando contraseña sin ocultar en login
+function revealPasswordLogin() {
+  const isPassword = loginPwd.type == "password";
+
+  loginPwd.type = isPassword ? "text" : "password";
+  showPassword.classList.toggle("fa-eye");
+  showPassword.classList.toggle("fa-eye-slash");
+}
+
+//Mostrar la otra contraseña sin ocultar
+function revealPasswordRpt() {
   const isPassword = passwordRpt.type == "password";
 
   passwordRpt.type = isPassword ? "text" : "password";
   showPasswordRpt.classList.toggle("fa-eye");
   showPasswordRpt.classList.toggle("fa-eye-slash");
-});
-
-//Show password in login
-loginIcon.addEventListener("click", () => {
-  const isPassword = loginPwd.type == "password";
-
-  loginPwd.type = isPassword ? "text" : "password";
-  loginIcon.classList.toggle("fa-eye");
-  loginIcon.classList.toggle("fa-eye-slash");
-})
-
-//Dirigirse al componente de registro
-btnSignUp.addEventListener("click", () => {
-  login.style.display = "none";
-  signUp.style.display = "flex";
-})
+}
 
 //Animación
 AOS.init({
