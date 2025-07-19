3a6fcdcbe2071ac00b3e668c8cd348843ceb8b67
Melanie Alcocer <melanieatr2005@gmail>
2025-07-18 21:47:33 -0600

    Anadiendo un comentario al index

---
+++ index.js
@@ -0,0 +1,66 @@
+//Prueba de commit
+
+//REGISTRO
+const signUp = document.getElementById("sign-up");
+const login = document.getElementById("log-in");
+const password = document.getElementById("contraseña");
+const passwordRpt = document.getElementById("contraseña-rpt");
+const showPassword = document.getElementById("eye-slash");
+const showPasswordRpt = document.getElementById("eye-slash-rpt");
+const loginPwd = document.getElementById("login-pwd");
+const inputsRegister = document.querySelectorAll(".inputs-register");
+const inputsLogin = document.querySelectorAll(".inputs-login");
+
+//Mostrando el login
+function showLogin() {
+  signUp.style.display = "none";
+  login.style.display = "block";
+
+  inputsRegister.forEach((input) => {
+    input.value = "";
+  });
+}
+
+//Dirigirse al componente de registro
+function showRegister() {
+  login.style.display = "none";
+  signUp.style.display = "flex";
+
+  inputsLogin.forEach((input) => {
+    input.value = "";
+  });
+}
+
+//Mostrando contraseña sin ocultar
+function revealPassword() {
+  const isPassword = password.type == "password";
+
+  password.type = isPassword ? "text" : "password";
+  showPassword.classList.toggle("fa-eye");
+  showPassword.classList.toggle("fa-eye-slash");
+}
+
+//Mostrando contraseña sin ocultar en login
+function revealPasswordLogin() {
+  const isPassword = loginPwd.type == "password";
+
+  loginPwd.type = isPassword ? "text" : "password";
+  showPassword.classList.toggle("fa-eye");
+  showPassword.classList.toggle("fa-eye-slash");
+}
+
+//Mostrar la otra contraseña sin ocultar
+function revealPasswordRpt() {
+  const isPassword = passwordRpt.type == "password";
+
+  passwordRpt.type = isPassword ? "text" : "password";
+  showPasswordRpt.classList.toggle("fa-eye");
+  showPasswordRpt.classList.toggle("fa-eye-slash");
+}
+
+//Animación
+AOS.init({
+  once: true,
+});
+
+
