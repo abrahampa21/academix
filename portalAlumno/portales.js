5129eec31402fbb34e50a42bad6ea61a5cc7cdc0
Abraham Pech <abrahamayil415@gmail.com>
2025-07-20 11:27:26 -0600

    Haciendo el sitio web responsivo para cualquier dispositivo

--- portalAlumno/portales.js
+++ portalAlumno/portales.js
@@ -1,5 +1,14 @@
 const pencil = document.querySelectorAll("i[data-input]");
+const btnTop = document.getElementById("scrollTopBtn");
+const toggle = document.getElementById("toggle");
+const menu = document.getElementById("menu");
+const bar = document.getElementById("bars");
 
+document.addEventListener("DOMContentLoaded", () =>{
+  btnTop.style.display = "none";
+})
+
+//Poder editar los datos de la tabla
 pencil.forEach((pen) => {
   pen.addEventListener("click", () => {
     const inputId = pen.getAttribute("data-input");
@@ -9,5 +18,34 @@ pencil.forEach((pen) => {
     }
   });
 });
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  const triggerElement = document.getElementById("about-us");
+
+  if (triggerElement.getBoundingClientRect().top < window.innerHeight / 2) {
+    btnTop.style.display = "block";
+  } else {
+    btnTop.style.display = "none";
+  }
+}
+
+// Función para volver arriba
+document.getElementById("scrollTopBtn").addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+
+//Mostrar el menú con la responsividad
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("menu-open");
+  const barOpen = menu.classList.contains("menu-open");
+
+  bar.classList = barOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+});
+
 
 AOS.init();
