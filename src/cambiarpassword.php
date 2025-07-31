5b1d776d2297f69e660bde74c6b126687f22c937
Melanie Alcocer <melanieatr2005@gmail.com>
2025-07-31 12:03:42 -0600

    Anadiendo cambios para cambiar contrasena del profe

--- src/cambiarpassword.php
+++ src/cambiarpassword.php
@@ -5,7 +5,8 @@
     $p2 =$_POST['p2'];
     if($p1 == $p2){
         $p1=sha1($p1);
-        $conexion->query("update alumno set password='$p1' where email='$email' ")or die($conexion->error);
+        $conexion->query("update alumno set password='$p1' where email='$email' ") or die($conexion->error);
+        $conexion->query("update profesor set password='$p1' where email='$email' ") or die($conexion->error);
         header("Location: ../login.php");
         
     }else{
