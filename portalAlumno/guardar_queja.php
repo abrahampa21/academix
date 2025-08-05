6cc044a5fb1ae6cf009f012245feb2fb2c4880e3
Melanie Alcocer <melanieatr2005@gmail.com>
2025-08-05 14:49:52 -0600

    agregando archivos php

---
+++ portalAlumno/guardar_queja.php
@@ -0,0 +1,18 @@
+<?php
+include '../src/conexion.php';
+
+$matriculaA = $_POST['matriculaA'];
+$matriculaP = $_POST['matriculaP'];
+$asunto = $_POST['asunto'];
+$mensaje = $_POST['mensaje'];
+
+$sql = "INSERT INTO quejas (matriculaA, matriculaP, asunto, mensaje) 
+        VALUES ('$matriculaA', '$matriculaP', '$asunto', '$mensaje')";
+
+if (mysqli_query($conn, $sql)) {
+    header("Location: portalAlumno.php");
+    exit();
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+?>
