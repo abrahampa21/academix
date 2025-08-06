8fd8ad699265235b8ac19926335ca82081b43bd7
Melanie Alcocer <melanieatr2005@gmail.com>
2025-08-05 21:06:27 -0600

    ya me harte

--- portalAlumno/guardar_queja.php
+++ portalAlumno/guardar_queja.php
@@ -1,19 +1,24 @@
 <?php
 session_start();
 include '../src/conexion.php';
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+mysqli_set_charset($conn, "utf8");
 
 $matriculaA = $_POST['matriculaA'];
 $matriculaP = $_POST['matriculaP'];
 $asunto = $_POST['asunto'];
 $mensaje = $_POST['mensaje'];
 
-$sql = "INSERT INTO quejas (matriculaA, matriculaP, asunto, mensaje) 
+$sql = "INSERT INTO quejas (matriculaA, matriculaP, asunto, mensaje)
         VALUES ('$matriculaA', '$matriculaP', '$asunto', '$mensaje')";
 
 if (mysqli_query($conn, $sql)) {
-    header("Location: portalAlumno.php");
-    exit();
+    echo "<script>
+            alert('Reporte enviado con éxito');
+            window.location.href = '../portalAlumno.php';
+          </script>";
 } else {
     echo "Error: " . mysqli_error($conn);
 }
 ?>
+
