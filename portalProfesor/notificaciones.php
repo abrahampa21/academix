c37dfdfbf8141e22524252486354159d2fbff8d8
Melanie Alcocer <melanieatr2005@gmail.com>
2025-08-06 22:37:03 -0600

    anadiendo bandeja de reportes al profe

--- portalProfesor/notificaciones.php
+++ portalProfesor/notificaciones.php
@@ -46,6 +46,10 @@ $result = $stmt->get_result();
     }
     select {
         padding: 5px;
+        font-size: 16px;
+  border-radius: 5px;
+  background-color: #e2e8f0;
+  color: #000;
     }
     button[type="submit"] {
         padding: 6px 12px;
@@ -53,6 +57,13 @@ $result = $stmt->get_result();
         color: white;
         border: none;
         cursor: pointer;
+
+        font-family: "Plus Jakarta Sans", sans-serif;
+  border-radius: 3px;
+  background-color: #1a3d7c;
+  color: #fff;
+  transition: 0.3s;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
     }
   </style>
 </head>
