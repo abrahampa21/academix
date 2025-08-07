b3c524838e2c03e378c390adc5999cbc827324f8
Melanie Alcocer <melanieatr2005@gmail.com>
2025-08-07 10:42:58 -0600

    borrado input de matricula

--- portalAlumno/pagos.php
+++ portalAlumno/pagos.php
@@ -93,14 +93,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
           id=""
           required
           />
+
+<?php
+if (!isset($_SESSION['id_matricula'])) {
+    echo "<script>alert('Sesión expirada o no iniciada. Vuelve a iniciar sesión'); window.location.href='../login.php';</script>";
+    exit;
+}
+?>
+
           <input 
-          type="hidden"
-          name="matriculaA"
-          title="u"
-          placeholder="Escriba su matricula"
-          id=""
-          required
-          />
+  type="hidden"
+  name="matriculaA"
+  value="<?php echo $_SESSION['id_matricula']; ?>"
+/>
+
           <input
           type="number"
           name="matriculaP"
